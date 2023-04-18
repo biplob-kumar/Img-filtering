e5440513b0844a1712c98ebdae5797477b36d60c
biplob kumar <biplobkumar125@gmail.com>
2023-04-18 11:38:13 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import { GalleryData } from './GallaryData';
 
 function App() {
+
+  const [data,setData]=useState([])
+  const [collection ,setCollection]=useState([])
+
+  useEffect(()=>{
+    setData(GalleryData)
+    setCollection([... new Set(GalleryData.map((item)=> item.titile))])
+ 
+  },[]) 
+
+  const gallary_filter=(itemData)=>{
+    const filterData=GalleryData.filter((item)=>item.titile==itemData)
+    setData(filterData);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <div className='gallary-wraper'>
+      <h2> Img Filtering</h2>
+      <div className='filter-item'>
+          <ul>
+            <li> <button onClick={()=> setData(GalleryData)} >All</button></li>
+            {
+              collection.map((item)=> <button  onClick={()=>{gallary_filter(item)}} >{item}</button>)
+            }
+          </ul>
+      </div>
+      <div className='gallary-continer'>
+      {
+        data.map((item)=>  <div key={item.id} className='gallary-item'> <img src={item.image} /></div> )
+      }
+      </div>
+
+      </div>
+         {/* <GalleryData/> */}
     </div>
   );
 }
---
+++ src/GallaryData.js
@@ -0,0 +1,61 @@
+export const GalleryData = [
+    {
+        id:1,
+        titile:"Car",
+        image:"./images/a1.jpg"
+    },
+    {
+        id:2,
+        titile:"Car",
+        image:"./images/a2.jpg"
+    },
+    {
+        id:3,
+        titile:"Car",
+        image:"./images/a3.jpg"
+    },
+
+    {
+        id:4,
+        titile:"Car",
+        image:"./images/a4.jpg"
+    },
+    {
+        id:5,
+        titile:"Car",
+        image:"./images/a5.jpg"
+    },
+    {
+        id:6,
+        titile:"Car",
+        image:"./images/a6.jpg"
+    },
+    {
+        id:7,
+        titile:"Car",
+        image:"./images/a7.jpg"
+    },
+
+
+    {
+        id:9,
+        titile:"Bike",
+        image:"./images/b1.jpg"
+    },
+    {
+        id:10,
+        titile:"Bike",
+        image:"./images/b2.jpg"
+    },
+    {
+        id:11,
+        titile:"Cycle",
+        image:"./images/c1.jpg"
+    },
+    {
+        id:12,
+        titile:"Cycle",
+        image:"./images/c2.jpg"
+    },
+
+]
